f426903cc8ff9b7188dd108fe39f175697c4152e
Daniel Berteaud <daniel@firewall-services.com>
2015-07-20 12:18:28 +0200

    Move UPS detection in a module

---
+++ lib/Zabbix/Agent/Addons/UPS.pm
@@ -0,0 +1,18 @@
+package Zabbix::Agent::Addons::UPS;
+
+use warnings;
+use strict;
+use File::Which;
+
+# List configured UPS (only nut is supported)
+sub list_ups {
+  my @ups = ();
+  my $upsc = which('upsc');
+  if ($upsc && -x $upsc){
+    my @out = qx($upsc -l);
+    if ($? == 0){
+      @ups = @out;
+    }
+  }
+  return @ups;
+};
