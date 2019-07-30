a66e9bb827967d509bd1f9c2669dcc4ff48438ff
Daniel Berteaud <daniel@firewall-services.com>
2019-07-30 13:59:42 +0200

    Fix when running on Debian buster Which fails with RC 25 when using File::Spec devnull

--- lib/Zabbix/Agent/Addons/UPS.pm
+++ lib/Zabbix/Agent/Addons/UPS.pm
@@ -3,15 +3,13 @@ package Zabbix::Agent::Addons::UPS;
 use warnings;
 use strict;
 use File::Which;
-use File::Spec;
-open STDERR, '>', File::Spec->devnull() or die "could not open STDERR: $!\n";
 
 # List configured UPS (only nut is supported)
 sub list_ups {
   my @ups = ();
   my $upsc = which('upsc');
   if ($upsc && -x $upsc){
-    my @out = qx($upsc -l);
+    my @out = qx($upsc -l 2>/dev/null);
     if ($? == 0){
       @ups = @out;
       chomp @ups;
